0a85d15683ab09f635be40ac9d68c5ede041979f
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-11-06 16:41:10 +0100

    Init project

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('picture')
+            ->add('title')
+            ->add('content')
+            ->add('publicationDate')
+            ->add('lastUpdateDate')
+            ->add('isPublished')
+            ->add('categories')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
